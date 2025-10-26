42f31afdd82738fc998e3756da238946cdafac75
Troy Edmonds <troydonte93@gmail.com>
2025-10-26 07:39:12 -0400

    Add files via upload

---
+++ Interactive-Exercises/Week-1/simple_loop4.py
@@ -0,0 +1,15 @@
+# This program demostrates how the range
+# function can be used with the for loop
+
+def main():
+    # Print a message five times.
+    for x in range(5):
+        print('Hello world')
+
+
+# Call the main function.
+if __name__ == '__main__':
+    main()
+
+
+
