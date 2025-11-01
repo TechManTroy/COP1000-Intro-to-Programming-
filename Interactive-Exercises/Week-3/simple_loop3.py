f72849ff0152125d704cf5b6cd9c008328bdd330
Troy Edmonds <troydonte93@gmail.com>
2025-11-01 11:30:49 -0400

    Add files via upload

---
+++ Interactive-Exercises/Week-3/simple_loop3.py
@@ -0,0 +1,9 @@
+# This program demonstrates a simple for loop
+# that uses a list of strings.
+
+def main():
+    print ('I will display the odd numbers 1 through 9.')
+    for name in ['Winken', 'Blinken', 'Nod']:
+        print(name)
+
+main()
