9a961916cba7c8400364d4dfb475efdd5c27943f
Troy Edmonds <troydonte93@gmail.com>
2025-11-01 16:39:44 -0400

    Add files via upload

---
+++ Interactive-Exercises/Week-3/rectangluar_pattern.py
@@ -0,0 +1,15 @@
+# This prgram displays a retangluar pattern
+# of asterisks
+
+
+
+def main():
+    rows = int(input('How many rows? '))
+    cols = int(input('How many columns? '))
+
+
+    for r in range (rows):
+        for c in range (cols):
+            print('*' end='')
+        print()    
+main()
