a27204ecefbb6afc07747dc8f32370e6cf4a6249
Troy Edmonds <troydonte93@gmail.com>
2025-10-31 22:07:24 -0400

    Add files via upload

---
+++ Practice-programs/Week-2/noninteractive_checkpoint.py
@@ -0,0 +1,18 @@
+
+
+
+def main():
+    number = int(input('Enter a sigle digit number: '))
+                 
+
+
+    if number == 1:
+        print('One')
+    elif number == 2:
+        print('Two')
+    elif number == 3:
+        print('Three')
+    else:
+        print('Unknown')
+
+main()        
