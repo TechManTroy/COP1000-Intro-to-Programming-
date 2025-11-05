32f3e39f24b7b6cc59cd75221b0c43e315e900b2
Troy Edmonds <troydonte93@gmail.com>
2025-11-05 13:59:36 -0500

    Add files via upload

---
+++ Interactive-Exercises/Week-4/birds.py
@@ -0,0 +1,27 @@
+# This program demonstrates two functions that
+# have local variables with the same name.
+
+def main():
+    # Call the taxeas funtion.
+    texas()
+    # Call the california function.
+    california()
+
+
+# Definition of the texas function. It creates
+# a local variable named birds.
+
+def texas():
+    birds = 5000
+    print(f'texas has {birds} birds.')
+
+
+# Definition of the texas function. It also creates
+# a local variable named birds.
+
+def california():
+    birds = 8000
+    print(f'california has {birds} birds.')
+
+# Call the main function.
+main()
