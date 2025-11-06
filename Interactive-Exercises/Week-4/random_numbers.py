3d5338cf88eae01ee21e43e2964a21798c5fe5e2
Troy Edmonds <troydonte93@gmail.com>
2025-11-06 12:49:02 -0500

    Add files via upload

---
+++ Interactive-Exercises/Week-4/random_numbers.py
@@ -0,0 +1,17 @@
+
+# This program displays a random number
+# in the range of 1 through 10
+import random
+
+def main():
+    # Get the random number.
+    number = random.randint(1,10)
+
+    # Display the number.
+    print(f'The number is {number}.')
+
+# Call the main function
+main()
+
+    
+    
