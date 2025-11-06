b1b2f2d8bbcd0b16fef938afe8179161e09dbca0
Troy Edmonds <troydonte93@gmail.com>
2025-11-06 12:51:17 -0500

    Add files via upload

---
+++ Interactive-Exercises/Week-4/random_numbers3.py
@@ -0,0 +1,14 @@
+
+# This program displays 5 random numbers
+# in the range of 1 through 100
+import random
+
+def main():
+    for count in range(5):
+        print(random.randint(1,100))
+        
+# Call the main function
+main()
+
+    
+    
