99f197a040dcd07074bf0ca17a10488468a585c2
Troy Edmonds <troydonte93@gmail.com>
2025-11-13 19:50:36 -0500

    Add files via upload

---
+++ Interactive-Exercises/Week-5/file_write.py
@@ -0,0 +1,24 @@
+# This program writes three lines of data
+# to a file.
+
+def main():
+
+    # Open a dile named philosophers.txt
+
+    outfile = open('philosophers.txt', 'w')
+
+
+    # Write the names of three pholosophers
+    # to the file
+    outfile.write('John Locke\n')
+    outfile.write('David Hume\n')
+    outfile.write('Edmund Burke\n')
+
+    # Close the file
+    outfile.close()
+
+# Call the main function
+if __name__ == "__main__":
+    main()
+    
+    
